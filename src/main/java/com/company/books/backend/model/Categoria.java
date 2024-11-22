640ffccca1c741789a1f63d39a1260ac35fd94b6
Javier Ugarrio <j.ugarrio.duro@gmail.com>
2024-11-22 11:43:59 +0100

    clase e interfaz para saber que en estado esta el servicio o la petcion

--- src/main/java/com/company/books/backend/model/Categoria.java
+++ src/main/java/com/company/books/backend/model/Categoria.java
@@ -12,10 +12,11 @@ public class Categoria implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -2164553723990982332L;// Identificador único para la versión serializada de la clase..Ayuda a guardar y recuperar objetos correctamente.
-	private Long id;
-	//
+	
 	@Id // Marca el campo como la clave primaria de la tabla.
 	@GeneratedValue(strategy = GenerationType.IDENTITY)// Indica que el valor del ID será generado automáticamente por la base de datos (auto incremento)
+	private Long id;
+	
 	private String nombre;
 	private String descripcion;
 	
